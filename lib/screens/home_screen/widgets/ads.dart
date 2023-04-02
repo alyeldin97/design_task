9568beaf1ff3be4892216cf5d2b3d19bc13d10ce
Aly Eldin Muhammad <aly@Alys-MacBook-Air.local>
2023-04-02 17:00:39 +0200

    completed reorder section

--- lib/screens/home_screen/widgets/ads.dart
+++ lib/screens/home_screen/widgets/ads.dart
@@ -34,109 +34,114 @@ class _AdsState extends State<Ads> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          height: 150.h,
-          width: 345.w,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15.r),
-          ),
-          child: CarouselSlider(
-            carouselController: carouselController,
-            options: CarouselOptions(
-                autoPlayInterval: Duration(seconds: 2),
-                autoPlay: true,
-                aspectRatio: 20 / 10,
-                viewportFraction: 1,
-                height: double.infinity,
-                disableCenter: true,
-                onPageChanged: (index, reason) {
-                  if (controller != null) {
-                    if (index == 0) {
-                      controller!.jump(0.1);
-                    } else if (index == 1) {
-                      controller!.jump(0.5);
-                    } else {
-                      controller!.jump(0.9);
-                    }
-                  }
-                }),
-            items: [
-              AppPadding(
-                start: 10,
-                child: Container(
-                  height: 361.h,
-                  width: 324.w,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15.r),
-                      image: DecorationImage(
-                          image: AssetImage(AssetImagesPaths.sale),
-                          fit: BoxFit.cover)),
-                ),
-              ),
-              AppPadding(
-                start: 10,
-                child: Container(
-                  height: 361.h,
-                  width: 324.w,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15.r),
-                      image: DecorationImage(
-                          image: AssetImage(AssetImagesPaths.cookdoor),
-                          fit: BoxFit.cover)),
-                ),
-              ),
-              AppPadding(
-                start: 10,
-                child: Container(
-                  height: 361.h,
-                  width: 324.w,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15.r),
-                      image: DecorationImage(
-                          image: AssetImage(AssetImagesPaths.spectra),
-                          fit: BoxFit.cover)),
-                ),
-              ),
-            ],
-          ),
-        ),
-        ActionSlider.standard(
-          sliderBehavior: SliderBehavior.move,
-          stateChangeCallback: (state, state2, controller) {
-            double position = double.parse(state2.position.toStringAsFixed(2));
-            bool isFromAutomaticSlide =
-                position == 0.1 || position == 0.5 || position == 0.9;
-
-            if (!isFromAutomaticSlide && (position > 0.0 && position < 0.29)) {
-              carouselController.jumpToPage(0);
-            } else if (!isFromAutomaticSlide &&
-                position > 0.29 &&
-                position < 0.6) {
-              carouselController.jumpToPage(1);
-            } else if (!isFromAutomaticSlide &&
-                position > 0.6 &&
-                position < 1.0) {
-              carouselController.jumpToPage(2);
-            }
-          },
-          reverseSlideAnimationDuration: Duration(seconds: 100000),
-          rolling: false,
-          height: 20.h,
-          width: 50.w,
-          backgroundColor: AppColors.grey,
-          boxShadow: [],
-          controller: controller,
-          icon: Container(
-            width: 30.w,
-            height: 10.h,
+    return AppPadding(
+      top: 10,
+      child: Column(
+        children: [
+          Container(
+            height: 150.h,
+            width: 345.w,
             decoration: BoxDecoration(
-                color: AppColors.green,
-                borderRadius: BorderRadius.circular(10.r)),
+              borderRadius: BorderRadius.circular(15.r),
+            ),
+            child: CarouselSlider(
+              carouselController: carouselController,
+              options: CarouselOptions(
+                  autoPlayInterval: Duration(seconds: 2),
+                  autoPlay: true,
+                  aspectRatio: 20 / 10,
+                  viewportFraction: 1,
+                  height: double.infinity,
+                  disableCenter: true,
+                  onPageChanged: (index, reason) {
+                    if (controller != null) {
+                      if (index == 0) {
+                        controller!.jump(0.1);
+                      } else if (index == 1) {
+                        controller!.jump(0.5);
+                      } else {
+                        controller!.jump(0.9);
+                      }
+                    }
+                  }),
+              items: [
+                AppPadding(
+                  start: 10,
+                  child: Container(
+                    height: 361.h,
+                    width: 324.w,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15.r),
+                        image: DecorationImage(
+                            image: AssetImage(AssetImagesPaths.sale),
+                            fit: BoxFit.cover)),
+                  ),
+                ),
+                AppPadding(
+                  start: 10,
+                  child: Container(
+                    height: 361.h,
+                    width: 324.w,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15.r),
+                        image: DecorationImage(
+                            image: AssetImage(AssetImagesPaths.cookdoor),
+                            fit: BoxFit.cover)),
+                  ),
+                ),
+                AppPadding(
+                  start: 10,
+                  child: Container(
+                    height: 361.h,
+                    width: 324.w,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15.r),
+                        image: DecorationImage(
+                            image: AssetImage(AssetImagesPaths.spectra),
+                            fit: BoxFit.cover)),
+                  ),
+                ),
+              ],
+            ),
           ),
-        )
-      ],
+          ActionSlider.standard(
+            sliderBehavior: SliderBehavior.move,
+            stateChangeCallback: (state, state2, controller) {
+              double position =
+                  double.parse(state2.position.toStringAsFixed(2));
+              bool isFromAutomaticSlide =
+                  position == 0.1 || position == 0.5 || position == 0.9;
+
+              if (!isFromAutomaticSlide &&
+                  (position > 0.0 && position < 0.29)) {
+                carouselController.jumpToPage(0);
+              } else if (!isFromAutomaticSlide &&
+                  position > 0.29 &&
+                  position < 0.6) {
+                carouselController.jumpToPage(1);
+              } else if (!isFromAutomaticSlide &&
+                  position > 0.6 &&
+                  position < 1.0) {
+                carouselController.jumpToPage(2);
+              }
+            },
+            reverseSlideAnimationDuration: Duration(seconds: 100000),
+            rolling: false,
+            height: 20.h,
+            width: 50.w,
+            backgroundColor: AppColors.grey,
+            boxShadow: [],
+            controller: controller,
+            icon: Container(
+              width: 30.w,
+              height: 10.h,
+              decoration: BoxDecoration(
+                  color: AppColors.green,
+                  borderRadius: BorderRadius.circular(10.r)),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/screens/home_screen/widgets/reorder.dart
+++ lib/screens/home_screen/widgets/reorder.dart
@@ -17,42 +17,77 @@ class ReOrder extends StatelessWidget {
 
   List descriptions = [
     'وجبة العاءلة',
-    '',
+    'متجر السلام',
   ];
   ReOrder({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        PrimaryTextRegular(text: 'اعادة الطلب', fontSize: 21),
-        Container(
-          height: 100.h,
-          child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: 2,
-              itemBuilder: (context, index) {
-                return AppPadding(
-                  start: 15,
-                  top: 15,
-                  child: Container(
-                    height: 100.h,
-                    width: 200.w,
-                    decoration: BoxDecoration(
-                        border: Border.all(color: AppColors.grey)),
-                    child: Column(
-                      children: [
-                        Row(
-                          children: [Image.asset(AssetImagesPaths.indian)],
-                        ),
-                      ],
+    return AppPadding(
+      top: 20,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          PrimaryTextRegular(text: 'اعادة الطلب', fontSize: 21),
+          Container(
+            height: 100.h,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 2,
+                itemBuilder: (context, index) {
+                  return AppPadding(
+                    start: 15,
+                    top: 15,
+                    child: Container(
+                      height: 100.h,
+                      width: 200.w,
+                      decoration: BoxDecoration(
+                          border: Border.all(color: AppColors.grey)),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Image.asset(images[index]),
+                              AppPadding(
+                                  start: 10,
+                                  child: PrimaryTextRegular(
+                                      text: names[index], fontSize: 14))
+                            ],
+                          ),
+                          AppPadding(
+                            start: 40,
+                            child: PrimaryTextRegular(
+                              text: descriptions[index],
+                              fontSize: 14,
+                              color: AppColors.secondaryColor.withOpacity(0.4),
+                            ),
+                          ),
+                          AppPadding(
+                            top: 20,
+                            child: Row(
+                              children: [
+                                AppPadding(
+                                    start: 100,
+                                    end: 5,
+                                    child:
+                                        Image.asset(AssetImagesPaths.reload)),
+                                PrimaryTextRegular(
+                                  text: 'اعادة الطلب',
+                                  fontSize: 12,
+                                  color: AppColors.green,
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                );
-              }),
-        )
-      ],
+                  );
+                }),
+          )
+        ],
+      ),
     );
   }
 }
